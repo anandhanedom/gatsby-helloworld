b91e866a06132f1c7800221819c6b2f4148cdcb6
Anandhan <anandhanedom@gmail.com>
2021-03-25 20:53:06 +0530

    Backup files

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout";
 const Home = () => {
   return (
     <Layout>
-      <h1>Hola</h1>
+      <h1>Hola!</h1>
       <h2>
         Don't miss all the news of the best league in the world. The single
         place for all LaLiga Santander football.
