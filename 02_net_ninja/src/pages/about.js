d2132daea901e5881f21d554e1cdeb81f0acd7c4
Anandhan <anandhanedom@gmail.com>
2021-03-30 22:50:12 +0530

    routing

---
+++ 02_net_ninja/src/pages/about.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, nemo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, nemo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, nemo.
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ 02_net_ninja/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <section>
+      <div>
+        <h2>Design</h2>
+        <h3>Develop and Deploy</h3>
+        <p>UX Designer and web developer based in Barcelona</p>
+      </div>
+    </section>
+  );
+}
