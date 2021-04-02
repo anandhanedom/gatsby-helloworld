37486d2f1a04e287d0a33ef7963c459dfe7830a0
Anandhan <anandhanedom@gmail.com>
2021-04-02 23:27:45 +0530

    bitbucket

--- 01_gatsby_bootcamp/src/templates/blog.js
+++ 01_gatsby_bootcamp/src/templates/blog.js
@@ -2,6 +2,7 @@ import React from "react";
 import { graphql } from "gatsby";
 import { renderRichText } from "gatsby-source-contentful/rich-text";
 
+// Components
 import Layout from "../components/layout";
 import Head from "../components/head";
 
